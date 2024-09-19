a6c9fc68d449be45e91037c6d8cb654098dcb797
MBERQUOCDN <marcel.berquo@academico.ufpb.br>
2024-09-19 16:38:22 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -9,3 +9,18 @@ st.text('Isso é um texto normal')
 st.markdown('Texto em **negrito** ou _itálico_')
 #Utilização para guardar html
 st.markdown('[Isso é um texto com html](https://docs.streamlit.io/en/stable/api.html#display-text)',False)
+
+
+
+
+import pandas as pd
+import numpy as np
+#gerando um dataframe aleatório
+df = pd.DataFrame(
+    np.random.randn(15,10),
+    columns=('col_%d' % i for i in range(10))
+ )
+#tabelas interativas
+st.dataframe(df)
+#tabelas fixas
+st.table(df)
